8d0ec59a3465f858c89ec3d1654f1e2b2a8ea2af
Gustoon <135733758+Gustoon@users.noreply.github.com>
2024-04-06 10:46:40 +0200

    macos!!

--- PathMePy_Gustoon/PathMePy.py
+++ PathMePy_Gustoon/PathMePy.py
@@ -3,12 +3,10 @@ import platform
 import site
 import sys
 
-if platform.system() != "Windows" and platform.system() != "Linux" :
-    print("I don't have MAC OS so I don't know how to change Path")
-    sys.exit(1)
-
 def PathMePyDir(path):
     path = os.path.abspath(path)
+    if platform.system() != "Windows" and platform.system() != "Linux" :
+        os.system('export PATH=$PATH:' + path)
     if platform.system() == "Windows" :
         os.system('set Path="%Path%;' + path + '"')
     elif platform.system() == "Linux":
@@ -16,7 +14,18 @@ def PathMePyDir(path):
 
 def PathMePyUserScriptFolder():
     path = site.getusersitepackages()
+    if platform.system() != "Windows" and platform.system() != "Linux" :
+        os.system('export PATH=$PATH:' + path)
     if platform.system() == "Windows" :
         os.system('set Path="%Path%;' + path + '"')
     elif platform.system() == "Linux":
         os.system('export PATH=$PATH:' + path)
+
+Current_Path = os.environ.get("PATH")
+
+if platform.system() != "Windows" and platform.system() != "Linux" :
+    Current_Path_Formated = Current_Path.replace(":", "\n")
+if platform.system() == "Windows" :
+    Current_Path_Formated = Current_Path.replace(";", "\n")
+elif platform.system() == "Linux":
+    Current_Path_Formated = Current_Path.replace(":", "\n")
